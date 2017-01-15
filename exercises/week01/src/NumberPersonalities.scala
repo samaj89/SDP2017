475ca6cde29eb39a138d2677a1e88d7448675146
samaj89 <samajansen89@gmail.com>
2017-01-15 12:37:58 +0000

    Initial setup

---
+++ exercises/week01/src/NumberPersonalities.scala
@@ -0,0 +1,56 @@
+/**
+  * Created by samaj on 15/01/17.
+  */
+object NumberPersonalities {
+  val limit = 100
+
+  def main(args: Array[String]) {
+    for (i <- 1 to limit) {
+      println(i)
+    }
+  }
+
+  def isPrime(n: Int): Boolean = {
+    false
+  }
+
+  def isHappy(n: Int): Boolean = {
+    false
+  }
+
+  def isTriangular(n: Int): Boolean = {
+    false
+  }
+
+  def isSquare(n: Int): Boolean = {
+    false
+  }
+
+  def isSmug(n: Int): Boolean = {
+    false
+  }
+
+  def isHonest(n: Int): Boolean = {
+    false
+  }
+
+  def isPronic(n: Int): Boolean = {
+    false
+  }
+
+  def isDeficient(n: Int): Boolean = {
+    false
+  }
+
+  def isPerfect(n: Int): Boolean = {
+    false
+  }
+
+  def isAbundant(n: Int): Boolean = {
+    false
+  }
+
+  def sumOfPositiveDivisorsOf(n: Int): Int = {
+    -99
+  }
+}
