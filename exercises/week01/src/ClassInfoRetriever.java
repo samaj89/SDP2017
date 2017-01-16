f7622cf4e52605be0f7589d895de44f6159f6264
samaj89 <samajansen89@gmail.com>
2017-01-16 13:19:13 +0000

    Initial setup

---
+++ exercises/week01/src/ClassInfoRetriever.java
@@ -0,0 +1,17 @@
+/**
+ * Write a Java program that reads the name of a class from the command line and emits the
+ * interface of the class in Java syntax (interface or class, modifiers, constructors, methods,
+ * fields; no method bodies).
+ */
+
+public class ClassInfoRetriever {
+
+    private void launch() {
+
+    }
+
+    public static void main(String[] args) {
+        ClassInfoRetriever cir = new ClassInfoRetriever();
+        cir.launch();
+    }
+}
