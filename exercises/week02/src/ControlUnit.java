30aad214077eaa6cfe2b1da9b03ac7b1ecdc0e02
samaj89 <samajansen89@gmail.com>
2017-01-21 11:35:33 +0000

    Refactored ControlUnit to inject list of sensors in constructor

--- exercises/week02/src/ControlUnit.java
+++ exercises/week02/src/ControlUnit.java
@@ -2,18 +2,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ControlUnit {
+    private List<Sensor> sensors;
 
-  public void pollSensors() {
-    List<Sensor> sensors = new ArrayList<Sensor>();
-    sensors.add(new FireSensor("Lobby 1st floor"));
-    sensors.add(new SmokeSensor("In the auditorium"));
-
-    for (Sensor sensor : sensors) {
-      if (sensor.isTriggered()) {
-        System.out.println("A " + sensor.getSensorType() + " sensor was triggered at " + sensor.getLocation());
-      } else {
-        System.out.println("Polled " + sensor.getSensorType() + " at " + sensor.getLocation() + " successfully");
-      }
+    public ControlUnit(List<Sensor> sensors) {
+        this.sensors = sensors;
     }
-  }
+
+    public void pollSensors() {
+        for (Sensor sensor : sensors) {
+            if (sensor.isTriggered()) {
+                System.out.println("A " + sensor.getSensorType() + " sensor was triggered at " + sensor.getLocation());
+            } else {
+                System.out.println("Polled " + sensor.getSensorType() + " at " + sensor.getLocation() + " successfully");
+            }
+        }
+    }
 }
+
+/**
+ * Q3/5: The original pollSensors() method's responsibilities were to instantiate a list of sensors, instantiate sensors
+ * and them to the list of the sensors, then poll all the sensors in the list. By passing ControlUnit a list of sensors
+ * in the constructor, we can limit the responsibilities of pollSensors() to just polling all the sensors in the list
+ * that was passed in.
+ */
