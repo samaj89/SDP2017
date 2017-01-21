41fa36d965311526f4ed1434e1f3725da8d13f90
samaj89 <samajansen89@gmail.com>
2017-01-21 10:46:47 +0000

    Added testThatIsTriggeredReturnsTrueAroundFivePercentOfTheTime()

--- exercises/week02/test/FireSensorTest.java
+++ exercises/week02/test/FireSensorTest.java
@@ -1,33 +1,39 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class FireSensorTest {
 
   @Test
-  public void testThatIsTriggeredReturnsFalse() {
-    FireSensor sensor = new FireSensor();
-    boolean isTriggered = sensor.isTriggered();
-    assertEquals(false, isTriggered);
+  public void testThatIsTriggeredReturnsTrueAroundFivePercentOfTheTime() {
+    FireSensor sensor = new FireSensor("Lobby 1st floor");
+    int count = 0;
+    boolean isTriggered;
+    for (int i = 0; i < 1000; i++) {
+        isTriggered = sensor.isTriggered();
+        if (isTriggered) count++;
+    }
+    assertTrue(count > 40 && count < 60);
   }
 
   @Test
   public void testThatGetLocationReturnsNull() {
-    FireSensor sensor = new FireSensor();
+    FireSensor sensor = new FireSensor("Lobby 1st floor");
     String location = sensor.getLocation();
     assertEquals(null, location);
   }
 
   @Test
   public void testThatGetSensorTypeReturnsNull() {
-    FireSensor sensor = new FireSensor();
+    FireSensor sensor = new FireSensor("Lobby 1st floor");
     String sensorType = sensor.getSensorType();
     assertEquals(null, sensorType);
   }
 
   @Test
   public void testThatGetBatteryPercentageReturnsNegativeOne() {
-    FireSensor sensor = new FireSensor();
+    FireSensor sensor = new FireSensor("Lobby 1st floor");
     double batteryPercentage = sensor.getBatteryPercentage();
     assertEquals(-1.0, batteryPercentage, 0.01);
   }
