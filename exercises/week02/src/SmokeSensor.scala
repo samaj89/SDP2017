92a79f1149a3b7dfb92e1891f44c4ad3c06885f8
samaj89 <samajansen89@gmail.com>
2017-01-22 12:00:25 +0000

    Implemented SmokeSensor

--- exercises/week02/src/SmokeSensor.scala
+++ exercises/week02/src/SmokeSensor.scala
@@ -1,12 +1,17 @@
+import scala.util.Random
+
 class SmokeSensor(location: String) extends Sensor {
   private val sensorType: String = "Smoke"
   private var batteryPercentage: Double = 100.0
 
-  override def isTriggered: Boolean = false
+  override def isTriggered: Boolean = {
+    if (getBatteryPercentage != 0) batteryPercentage -= 20
+    Random.nextDouble() < 0.1
+  }
 
-  override def getLocation: String = null
+  override def getLocation: String = location
 
-  override def getSensorType: String = null
+  override def getSensorType: String = sensorType
 
-  override def getBatteryPercentage: Double = -1
+  override def getBatteryPercentage: Double = batteryPercentage
 }
