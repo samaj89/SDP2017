6b99c5bcb93655086a23f5716e1fe3f5a6066eb3
samaj89 <samajansen89@gmail.com>
2017-01-22 11:35:19 +0000

    Adding constructor, fields to FireSensor

--- exercises/week02/src/FireSensor.scala
+++ exercises/week02/src/FireSensor.scala
@@ -1,4 +1,7 @@
-class FireSensor extends Sensor {
+class FireSensor(location: String) extends Sensor {
+  private val sensorType: String = "Fire"
+  private var batteryPercentage: Double = 100.0
+
   override def isTriggered: Boolean = false
 
   override def getLocation: String = null
