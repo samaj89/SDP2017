27644a8ee37125bf03ea9526d7f7b34a06366371
samaj89 <samajansen89@gmail.com>
2017-01-29 11:13:27 +0000

    Implemented splitInHalf()

--- exercises/week03/src/main/scala/basics/ScalaBasics.scala
+++ exercises/week03/src/main/scala/basics/ScalaBasics.scala
@@ -133,7 +133,7 @@ object ScalaBasics {
    * @param s the string to split
    * @return the split string as a tuple
    */
-  def splitInHalf(s: String): (String, String) = ???
+  def splitInHalf(s: String): (String, String) = s splitAt s.length/2
 
   /**
    * Determines if the given string s is a palindrome.
