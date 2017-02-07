08519c38fce6348f09f18a74b66bed74e2c7b994
samaj89 <samajansen89@gmail.com>
2017-02-07 16:28:45 +0000

    Implemented SubInstruction execute(), all tests pass

--- coursework/cw-one/SML_Scala/src/sml/SubInstruction.scala
+++ coursework/cw-one/SML_Scala/src/sml/SubInstruction.scala
@@ -7,7 +7,11 @@ package sml
 class SubInstruction(label: String, op: String, val result: Int, val op1: Int, val op2: Int)
   extends Instruction(label, op) {
 
-  override def execute(m: Machine) = ???
+  override def execute(m: Machine) = {
+    val value1 = m.regs(op1)
+    val value2 = m.regs(op2)
+    m.regs(result) = value1 - value2
+  }
 
   override def toString(): String = {
     super.toString + " " + op1 + " - " + op2 + " to " + result
