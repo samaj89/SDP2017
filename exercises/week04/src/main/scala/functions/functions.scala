8b186138732df41f35718690aad41b0e4ae765dc
samaj89 <samajansen89@gmail.com>
2017-02-10 12:25:26 +0000

    Implemented sum(), all tests pass

--- exercises/week04/src/main/scala/functions/functions.scala
+++ exercises/week04/src/main/scala/functions/functions.scala
@@ -90,7 +90,7 @@ object Funcs {
     * the sublists into one long list. For example, flatten(List(List(1,2,3),
     * List(4,5,6))) produces List(1,2,3,4,5,6).
     */
-  def sum(ls: List[Double]): Double = ???
+  def sum(ls: List[Double]): Double = foldLeft(ls, 0.0)(_ + _)
 
   def product(ls: List[Double]): Double = ???
 
