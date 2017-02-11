71fd240ddfd14cadebf9885606bb3eb50b71fb31
samaj89 <samajansen89@gmail.com>
2017-02-11 09:57:21 +0000

    Implemented flatMap()

--- exercises/week04/src/main/scala/functions/functions.scala
+++ exercises/week04/src/main/scala/functions/functions.scala
@@ -139,7 +139,7 @@ object Funcs {
     * @return a List[B] containing the flattened results of applying f to all
     *         elements of ls.
     */
-  def flatMap[A, B](ls: List[A])(f: A => List[B]): List[B] = ???
+  def flatMap[A, B](ls: List[A])(f: A => List[B]): List[B] = flatten(map(ls)(f))
 
   // COMBINING FUNCTIONS
 
