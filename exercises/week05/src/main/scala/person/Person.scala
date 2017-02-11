9cc59997f285f14a1c564996abde435403513d9b
samaj89 <samajansen89@gmail.com>
2017-02-11 15:26:49 +0000

    Implemented companion object apply()

--- exercises/week05/src/main/scala/person/Person.scala
+++ exercises/week05/src/main/scala/person/Person.scala
@@ -5,5 +5,8 @@ class Person(val first: String, val last: String) {
 }
 
 object Person {
-  def apply(name: String) = ???
+  def apply(name: String) = {
+    val parts = name.split(" ")
+    new Person(parts(0), parts(1))
+  }
 }
