670e698fba669e45c5cfe86b6360e7ab0f9062e4
samaj89 <samajansen89@gmail.com>
2017-02-11 14:56:48 +0000

    Implemented adjust()

--- exercises/week05/src/main/scala/counter/Counter.scala
+++ exercises/week05/src/main/scala/counter/Counter.scala
@@ -5,6 +5,6 @@ case class Counter(count: Int = 0) {
 
   def dec(x: Int = 1): Counter = this.copy(count = count - x)
 
-  def adjust(adder: Adder): Counter = ???
+  def adjust(adder: Adder): Counter = this.copy(count = adder.add(count))
 }
 
