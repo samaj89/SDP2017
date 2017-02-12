e0e0bdf21b96fbb8be190a5a6e8ee874a86345b2
samaj89 <samajansen89@gmail.com>
2017-02-12 13:01:26 +0000

    Converted Director, Film to case classes, removed redundant apply(), copy(), getter methods

--- exercises/week05/src/main/scala/films/Director.scala
+++ exercises/week05/src/main/scala/films/Director.scala
@@ -1,15 +1,15 @@
 package films
 
-class Director(firstName: String, lastName: String, private val _yearOfBirth: Int) {
+case class Director(firstName: String, lastName: String, yearOfBirth: Int) {
 
   def name: String = firstName + " " + lastName
 
-  def yearOfBirth = _yearOfBirth
 }
 
 object Director {
-  def apply(firstName: String, lastName: String, yearOfBirth: Int) =
-    new Director(firstName, lastName, yearOfBirth)
 
   def older(d1: Director, d2: Director) = if (d1.yearOfBirth < d2.yearOfBirth) d1 else d2
+
 }
+
+
--- exercises/week05/src/main/scala/films/Film.scala
+++ exercises/week05/src/main/scala/films/Film.scala
@@ -1,27 +1,15 @@
 package films
 
-class Film(private val _name: String, private val _yearOfRelease: Int,
-           private val _imdbRating: Double, private val _director: Director) {
+case class Film(name: String, yearOfRelease: Int,
+           imdbRating: Double, director: Director) {
 
   def directorsAge: Int = yearOfRelease - director.yearOfBirth
 
   def isDirectedBy(director: Director): Boolean = director.equals(this.director)
 
-  def copy(name: String = this.name, yearOfRelease: Int = this.yearOfRelease,
-           imdbRating: Double = this.imdbRating, director: Director = this.director): Film =
-    new Film(name, yearOfRelease, imdbRating, director)
-
-  def name = _name
-  def yearOfRelease = _yearOfRelease
-  def imdbRating = _imdbRating
-  def director = _director
-
 }
 
 object Film {
-  def apply(name: String, yearOfRelease: Int, imdbRating: Double, director: Director) =
-    new Film(name, yearOfRelease, imdbRating, director)
-
   def highestRating(f1: Film, f2: Film): Double =
     if (f1.imdbRating > f2.imdbRating) f1.imdbRating else f2.imdbRating
 
