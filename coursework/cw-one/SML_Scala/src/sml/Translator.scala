dac6d751e6f362e2aba8c6917d5bca020f34e9ff
samaj89 <samajansen89@gmail.com>
2017-02-18 12:37:31 +0000

    Added BNZ case to Translator pattern match

--- coursework/cw-one/SML_Scala/src/sml/Translator.scala
+++ coursework/cw-one/SML_Scala/src/sml/Translator.scala
@@ -34,6 +34,8 @@ class Translator(fileName: String) {
             program = program :+ MulInstruction(fields(0), fields(2).toInt, fields(3).toInt, fields(4).toInt)
           case OUT =>
             program = program :+ OutInstruction(fields(0), fields(2).toInt)
+          case BNZ =>
+            program = program :+ BnzInstruction(fields(0), fields(2).toInt, fields(3))
           case x =>
             println(s"Unknown instruction $x")
         }
