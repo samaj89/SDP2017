5bc958e6d2232702091eea0a00d2569d5de35510
samaj89 <samajansen89@gmail.com>
2017-02-18 12:34:57 +0000

    Implemented BnzInstruction

--- coursework/cw-one/SML_Scala/src/sml/BnzInstruction.scala
+++ coursework/cw-one/SML_Scala/src/sml/BnzInstruction.scala
@@ -6,13 +6,22 @@ package sml
 class BnzInstruction(label: String, op: String, val op1: Int, goTo: String)
   extends Instruction(label, op){
 
-  override def execute(m: Machine) = ???
+  override def execute(m: Machine) = {
+    if (m.regs(op1) != 0) {
+      m.pc = m.labels.labels.indexOf(goTo)
+    }
+  }
 
-  override def toString(): String = ???
+  override def toString(): String =
+    super.toString + " if contents of " + op1 + " is not 0 branch to " + goTo + "\n"
 
 }
 
 object BnzInstruction {
   def apply(label: String, op1: Int, goTo: String) =
     new BnzInstruction(label, "bnz", op1, goTo)
+
+  def main(args: Array[String]): Unit = {
+    println(BnzInstruction("L1", 1, "L3").toString())
+  }
 }
