d12986eb4209ae37049afe31b64f5538c354aa70
samaj89 <samajansen89@gmail.com>
2017-02-18 12:52:18 +0000

    Added tests for DivInstruction

--- coursework/cw-one/SML_Scala/test/SMLTests.scala
+++ coursework/cw-one/SML_Scala/test/SMLTests.scala
@@ -106,4 +106,43 @@ class SMLTests {
     BnzInstruction("L3", 1, "L2").execute(m)
     assertTrue(m.pc == 2)
   }
+
+  @Test def DividePositiveByPositive() = {
+    LinInstruction("L!", 1, 10).execute(m)
+    LinInstruction("L2", 2, 2).execute(m)
+    DivInstruction("L3", 3, 1, 2)
+    assertTrue(m.regs(3) == 5)
+  }
+
+  @Test def DividePositiveByNegative() = {
+    LinInstruction("L!", 1, 10).execute(m)
+    LinInstruction("L2", 2, -2).execute(m)
+    DivInstruction("L3", 3, 1, 2)
+    assertTrue(m.regs(3) == -5)
+  }
+
+  @Test def DividingZeroReturnsZero() = {
+    LinInstruction("L1", 2, 2).execute(m)
+    DivInstruction("L2", 3, 1, 2)
+    assertTrue(m.regs(3) == 0)
+  }
+
+  @Test def MultiplyingThenDividingReturnsOriginal() = {
+    LinInstruction("L!", 1, 10).execute(m)
+    LinInstruction("L2", 2, 2).execute(m)
+    MulInstruction("L3", 3, 1, 2).execute(m)
+    DivInstruction("L4", 4, 3, 2).execute(m)
+    assertTrue(m.regs(4) == 10)
+  }
+
+  @Test def DividingRegisterContentsByItselfReturnsOne() = {
+    LinInstruction("L!", 1, 10).execute(m)
+    DivInstruction("L2", 1, 1, 1).execute(m)
+    assertTrue(m.regs(1) == 1)
+  }
+
+  @Test def DivInstructionToStringReflectsContents() = {
+    val output = DivInstruction("L3", 2, 1, 2).toString()
+    assertTrue(output == "L3: div 1 / 2 to 2\n")
+  }
 }
