00cf222100cb3b8dfd9a27e50f0fd0251da198ad
samaj89 <samajansen89@gmail.com>
2017-03-05 11:43:44 +0000

    Implemented CarDirector

--- exercises/week08/src/main/scala/builder/CarDirector.scala
+++ exercises/week08/src/main/scala/builder/CarDirector.scala
@@ -1,8 +1,14 @@
 package builder
 
-// TODO
-
 case class CarDirector(carBuilder: CarBuilder) {
-  def build = ???
+  def build = {
+    carBuilder.buildBodyStyle
+    carBuilder.buildBreaks
+    carBuilder.buildEngine
+    carBuilder.buildFuelType
+    carBuilder.buildPower
+    carBuilder.buildSeats
+    carBuilder.buildWindows
+  }
 
 }
