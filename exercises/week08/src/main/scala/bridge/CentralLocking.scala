3d18bc0659d44ed32d074a867bf3ba2cb66a436e
samaj89 <samajansen89@gmail.com>
2017-03-05 12:24:29 +0000

    Implemented CentralLocking, GearLocking

--- exercises/week08/src/main/scala/bridge/CentralLocking.scala
+++ exercises/week08/src/main/scala/bridge/CentralLocking.scala
@@ -1,8 +1,8 @@
 package bridge
 
 case class CentralLocking(s: String) extends Product {
-// TODO
-  override def productName: String = ???
 
-  override def produce: Unit = ???
+  override def productName: String = s
+
+  override def produce: Unit = println("Producing " + productName)
 }
--- exercises/week08/src/main/scala/bridge/GearLocking.scala
+++ exercises/week08/src/main/scala/bridge/GearLocking.scala
@@ -1,9 +1,8 @@
 package bridge
 
 case class GearLocking(s: String) extends Product {
-// TODO
 
-  override def productName: String = ???
+  override def productName: String = s
 
-  override def produce: Unit = ???
+  override def produce: Unit = println("Producing " + productName)
 }
