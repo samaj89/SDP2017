32ec07600825f3665b2464abb1278ba8a5ce6e35
samaj89 <samajansen89@gmail.com>
2017-03-05 12:52:18 +0000

    Implemented HtmlTag

--- exercises/week08/src/main/scala/composite/HtmlTag.scala
+++ exercises/week08/src/main/scala/composite/HtmlTag.scala
@@ -1,21 +1,28 @@
 package composite
 
+import java.util.ArrayList
 import java.util.List
 
 abstract class HtmlTag(tagName: String) {
+  var tagBody: String
+  var children = new ArrayList[HtmlTag]()
+
   def getTagName: String = tagName
 
   def setStartTag(tag: String)
 
   def setEndTag(tag: String)
 
-  def setTagBody(tagBody: String) = ???
+  def setTagBody(tagBody: String) = this.tagBody = tagBody
 
-  def addChildTag(htmlTag: HtmlTag) = ???
+  def addChildTag(htmlTag: HtmlTag) = children add htmlTag
 
-  def removeChildTag(htmlTag: HtmlTag) = ???
+  def removeChildTag(htmlTag: HtmlTag) = {
+    val index = children.indexOf(htmlTag)
+    if (index != -1) children remove index
+  }
 
-  def getChildren: List[HtmlTag] = ???
+  def getChildren: List[HtmlTag] = children
 
   def generateHtml
 }
