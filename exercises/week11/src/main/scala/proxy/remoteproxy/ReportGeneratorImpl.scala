46e7a15fa1ba056a7f1dcb59a0e9b70e40db616d
samaj89 <samajansen89@gmail.com>
2017-04-16 15:22:14 +0100

    Tweaked report string

--- exercises/week11/src/main/scala/proxy/remoteproxy/ReportGeneratorImpl.scala
+++ exercises/week11/src/main/scala/proxy/remoteproxy/ReportGeneratorImpl.scala
@@ -22,12 +22,12 @@ class ReportGeneratorImpl protected ()
   override def generateDailyReport(): String = {
     var sb: StringBuilder = new StringBuilder
     sb.append("********************Location X Daily Report********************")
-    sb.append("\\n Location ID: 012")
-    sb.append("\\n Today’s Date: Sun Sep 39 00:11:23 GMT 2016")
-    sb.append("\\n Total Pizza Sell: 112")
-    sb.append("\\n Total Sale: $2534")
-    sb.append("\\n Net Profit: $1985")
-    sb.append("\\n***************************************************************")
+    sb.append("\n Location ID: 012")
+    sb.append("\n Today’s Date: Sun Sep 39 00:11:23 GMT 2016")
+    sb.append("\n Total Pizza Sell: 112")
+    sb.append("\n Total Sale: $2534")
+    sb.append("\n Net Profit: $1985")
+    sb.append("\n***************************************************************")
 
     sb.toString
   }
