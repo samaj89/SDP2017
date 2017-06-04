03d3ccd3419528bb9cd9f37122bdf93ef6d63685
samaj89 <samajansen89@gmail.com>
2017-06-04 15:56:50 +0100

    Added tail recursive size, inorder traversal

--- revision/src/main/scala/trees/Tree.scala
+++ revision/src/main/scala/trees/Tree.scala
@@ -1,5 +1,7 @@
 package trees
 
+import scala.annotation.tailrec
+
 sealed trait Tree
 final case class Node(lhs: Tree, v: Int, rhs: Tree) extends Tree
 final case object Leaf extends Tree
@@ -18,6 +20,18 @@ object TreeOps extends App {
     case Node(lhs, _, rhs) => 1 + size(lhs) + size(rhs)
   }
 
+  def sizeTailRec(t: Tree): Int = {
+    @tailrec
+    def sizeAcc(lst: List[Tree], acc: Int): Int = lst match {
+      case Nil => acc
+      case Leaf :: t => sizeAcc(t, acc)
+      case Node(lhs, _, rhs) :: t => sizeAcc(lhs :: rhs :: t, acc + 1)
+    }
+
+    sizeAcc(List(t), 0)
+  }
+
+  @tailrec
   def find(n: Int, t: Tree): Boolean = t match {
     case Leaf => false
     case Node(lhs, v, rhs) if n == v => true
@@ -25,10 +39,17 @@ object TreeOps extends App {
     case Node (_, v, rhs) => find(n, rhs)
   }
 
+  def inOrderTraversal(t: Tree): String = t match {
+    case Leaf => ""
+    case Node(lhs, v, rhs) => inOrderTraversal(lhs) + " " + v.toString + " " + inOrderTraversal(rhs)
+  }
+
   val t1 = Node(Leaf, 10, Leaf)
   val t2 = insert(1, insert(12, insert(10, insert(23, insert(7, insert(15, insert(5, t1)))))))
   println(t2)
   println(size(t2))
+  println(sizeTailRec(t2))
   println(find(7, t2))
-  print(find(19, t2))
+  println(find(19, t2))
+  println(inOrderTraversal(t2))
 }
